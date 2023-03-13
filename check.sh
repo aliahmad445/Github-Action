acf6964bf3c37c5d03eb8514dba9788b3b4797c4
aliahmad445 <aliahmad445@yahoo.com>
2023-03-13 14:06:02 +0500

    Update check.sh

--- check.sh
+++ check.sh
@@ -11,7 +11,7 @@ echo "::warning file=app.js,line=1,col=5,endColumn=7::Missing semicolon"
 echo "action_state=yellow" >> $GITHUB_ENV
 echo "::warning file=app.js,line=1,col=5,endColumn=7::Missing semicolon" >> $GITHUB_STEP_SUMMARY
 echo "### Hello world! :rocket:" >> $GITHUB_STEP_SUMMARY
-echo "<style type="text/css"> h1 {color:red;} p {color:blue;} </style>" >> $GITHUB_STEP_SUMMARY
+echo "<dl><dt>Definition list</dt><dd>Is something people use sometimes.</dd><dt>Markdown in HTML</dt><dd style="color:Tomato;">Does *not* work **very** well. Use HTML <em>tags</em>.</dd></dl>" >> $GITHUB_STEP_SUMMARY
 okay
 exit 1
 fi
