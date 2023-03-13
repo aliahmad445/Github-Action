450d6297d05f71226948ab90ce01667467714451
aliahmad445 <aliahmad445@yahoo.com>
2023-03-13 13:16:06 +0500

    Update check.sh

--- check.sh
+++ check.sh
@@ -9,6 +9,7 @@ echo 1
 echo 2
 echo "::warning file=app.js,line=1,col=5,endColumn=7::Missing semicolon"
 echo "action_state=yellow" >> $GITHUB_ENV
+echo "::warning file=app.js,line=1,col=5,endColumn=7::Missing semicolon" >> $GITHUB_STEP_SUMMARY
 echo "### Hello world! :rocket:" >> $GITHUB_STEP_SUMMARY
 exit 1
 fi
