d330479e15a03da93dd0de73f9c572978e177d99
aliahmad445 <aliahmad445@yahoo.com>
2023-03-13 14:19:55 +0500

    Update check.sh

--- check.sh
+++ check.sh
@@ -9,7 +9,6 @@ echo 1
 echo 2
 echo "action_state=yellow" >> $GITHUB_ENV
 echo "<dd style="color:Tomato;">Does *not* work **very** well. Use HTML <em>tags</em>.</dd>" >> $GITHUB_STEP_SUMMARY
-okay
 exit 1
 fi
 echo tttttt
