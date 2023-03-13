62d1402cc432d49f0278698dda108cf0f1e9d20d
aliahmad445 <aliahmad445@yahoo.com>
2023-03-13 14:38:37 +0500

    Update check.sh

--- check.sh
+++ check.sh
@@ -9,6 +9,7 @@ echo 1
 echo 2
 echo "action_state=yellow" >> $GITHUB_ENV
 echo "This **word** is bold" >> $GITHUB_STEP_SUMMARY
+echo "Deployment Status: **Reached maximum retries(20), please check the pod's error log**" >> $GITHUB_STEP_SUMMARY
 exit 1
 fi
 echo tttttt
