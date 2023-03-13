dc51b456f2e55ce439e3e84961f808784c9e9e2c
aliahmad445 <aliahmad445@yahoo.com>
2023-03-13 14:27:26 +0500

    Update check.sh

--- check.sh
+++ check.sh
@@ -8,7 +8,7 @@ if [ $count = 0 ]; then
 echo 1
 echo 2
 echo "action_state=yellow" >> $GITHUB_ENV
-echo "<dd style="color:Tomato;">Does *not* work **very** well. Use HTML <em>tags</em>.</dd>" >> $GITHUB_STEP_SUMMARY
+echo "This **word** is bold" >> $GITHUB_STEP_SUMMARY
 exit 1
 fi
 echo tttttt
